e0d43e7d17ba23de4dcbbf0eacc490ff8ab375f9
Christoph Sax <christoph.sax@gmail.com>
2025-08-19 22:32:25 +0200

    multi expr summarize

--- R/pkg.R
+++ R/pkg.R
@@ -8,4 +8,8 @@
 #' @importFrom stats rnorm
 #' @importFrom jsonlite toJSON
 #' @importFrom shinyjs runjs
+#' @importFrom dplyr n n_distinct row_number first last nth
+#' @importFrom dplyr lag lead cummean cumall cumany
+#' @importFrom dplyr dense_rank min_rank percent_rank cume_dist ntile
+#' @importFrom dplyr between near
 NULL
